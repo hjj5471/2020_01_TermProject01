1d90fda651121e422741a37285999a983ace3b13
hjj54 <hjj54@LAPTOP-9T6U5P7B>
2020-05-18 21:58:07 +0900

    단어 추출

--- TermProject01/src/colling/movie.java
+++ TermProject01/src/colling/movie.java
@@ -1,4 +1,4 @@
-package crolling;
+package colling;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -8,36 +8,40 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import org.openqa.selenium.*;
-
 public class movie {
 	public static void main(String[] args) throws IOException{
 		
 		String url = "https://movie.naver.com/movie/sdb/rank/rmovie.nhn?sel=pnt&date=20200515";
 		ArrayList point_list = new ArrayList();
 		ArrayList page_list = new ArrayList();
-				
+		Word_Extraction EXT = new Word_Extraction();
+		
 		Document document = Jsoup.connect(url).get();
 		Elements movies = document.select("div.tit5");
 		
-		
-		//System.out.println(movies);
+		//System.out.println(movies); // 평점 URL 크롤링
 		for (Element movie : movies){
 			// System.out.println(movie.select("a"));
 			String moviepoint_url = movie.select("a").attr("href");
-			moviepoint_url = "https://movie.naver.com" + moviepoint_url.replace("basic","point");
+			moviepoint_url = "https://movie.naver.com" + moviepoint_url.replace("basic","pointWriteFormList");
 			point_list.add(moviepoint_url);
-			System.out.println(movie.text());
+//			System.out.println(movie.text());
+//			System.out.println(moviepoint_url);			
 		}
 		
-		for(int i = 0; i < point_list.size(); i++) {
-			Document doc = Jsoup.connect((String)point_list.get(i)).get();
+		String total_review = "";
 			
-			Elements page = doc.select(".paging");
-			System.out.println(page);
-			
-		
-			
+		for(int i = 49; i < point_list.size(); i++) {		// i = 영화 RANKING 번호		
+			for(int j = 1; j <= 15; j++) { 					// j = PAGE 번호
+//				System.out.println(getParameter((String)point_list.get(i), j));
+				Document doc = Jsoup.connect(getParameter((String)point_list.get(i), j)).get();
+				
+				Elements pages = doc.select("div.score_reple p span");
+				total_review = total_review + " " + pages.text();
+//				System.out.println(pages.text());
+			}
+			EXT.matext(total_review);
+			System.out.println("==========================================================================");
 		}
 //		
 //		Document doc = Jsoup.connect((String)movie_list.get(0)).get();
@@ -54,4 +58,5 @@ public class movie {
 				+ "&isMileageSubscriptionReject=false"
 				+ "&page=" + PAGE;
 	}
+//	https://movie.naver.com/movie/bi/mi/pointWriteFormList.nhn?code=171539&type=after&isActualPointWriteExecute=false&isMileageSubscriptionAlready=false&isMileageSubscriptionReject=false&page=2
 }
